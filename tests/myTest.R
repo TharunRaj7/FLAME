1ccd35d2e101c860a91db465cda0dbc80791cf75
Tharun Raj <tm272@duke.edu>
2020-07-20 03:43:06 +0800

    myTest.R updated

--- tests/myTest.R
+++ tests/myTest.R
@@ -1,5 +1,5 @@
-library(magrittr)
-library(glmnet)
+
+# Function to source files to avoid using devtools
 sourceFolder <- function(folder, recursive = FALSE, ...)
 {
   files <- list.files(folder, pattern = "[.][rR]$",
@@ -9,21 +9,25 @@ sourceFolder <- function(folder, recursive = FALSE, ...)
   src <- invisible(lapply(files, source, ...))
   message(sprintf('%s files sourced from folder "%s"', length(src), folder))
 }
-sourceFolder("C:/Users/traj1/Projects/R projects/FLAME/R")
-set.seed(45)
+sourceFolder("INCLUDE DIRECTORY HERE")
+#set.seed(45)
 
-user_PE_fit <- NULL
-user_PE_fit_params <- NULL
-user_PE_predict <- NULL
-user_PE_fit_params <- NULL
+
+# Testing with native funtions
+#####################################
 n <- 100
 p <- 5
 data <- gen_data(n, p) # Data we would like to match
 holdout <- gen_data(n, p) # Data we will train on, to compute PE
-result <- FLAME(data = data, holdout = holdout)
+FLAME_out <- FLAME(data = data, holdout = holdout)
 #print(result)
+####################################
+
+
+
 
 #Testing with custom functions
+###################################
 n <- 100
 p <- 5
 data <- gen_data(n, p) # Data we would like to match
@@ -36,3 +40,4 @@ my_predict <- function(bart_fit, new_data) {
 FLAME_out <-
   FLAME(data = data, holdout = holdout,
         user_PE_fit = my_fit, user_PE_fit_params = my_fit_params,user_PE_predict = my_predict)
+###################################
